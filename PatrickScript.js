deb3a0aa1cbabc3e575add0b3c736b7386adfd77
patrickbaquiran99-boop <patrickbaquiran99@gmail.com>
2025-09-15 22:38:46 +0800

    This is my main Focus

--- PatrickScript.js
+++ PatrickScript.js
@@ -1,11 +1,25 @@
-let text = document.getElementById('text');
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrlIcon = document.getElementById("ctrlIcon");
+
+song.onloadedmetadata = function() {
+
+progress.max = song.duration;
+progress.value = song.currentTime;
+}
+
+function playPause(){
+if(ctrlIcon.classList.contains("fa-pause")){
+    song.pause();
+    ctrlIcon.classList.remove("fa-pause");
+    ctrlIcon.classList.add("fa-play");
+}
+else{
+
+song.play();
+ctrlIcon.classList.add("fa-pause");
+    ctrlIcon.classList.remove("fa-play");
+}
 
 
-
-window.addEventListener('scroll', () => {
-let value = window.scrollY;
-
-text.style.marginTop = value * 2.5 + 'px';
-
-
-});
+}
